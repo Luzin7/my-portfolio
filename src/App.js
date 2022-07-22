127b6b4e81ab21930bdeba11c8d0f39fffe8aeec
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-22 11:08:09 -0300

    feat: Add projects section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div id="home" className="App">
       <Header />
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
