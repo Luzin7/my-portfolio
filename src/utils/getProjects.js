71470a97099f44e56c8467a9255c8c0eeb2ba9d5
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-22 11:05:33 -0300

    feat: Create getProjects function
    Return objetcs that contains infos about my projects

---
+++ src/utils/getProjects.js
@@ -0,0 +1,20 @@
+export const PROJECTS = [
+  {
+    id: 1,
+    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    name: "Game-hub",
+    repository: "url",
+  },
+  {
+    id: 2,
+    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    name: "Weather App",
+    repository: "url",
+  },
+  {
+    id: 3,
+    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    name: "Em breve",
+    repository: "Em breve",
+  },
+];
