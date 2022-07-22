5c377fe3b6aec90d778ebc76e222ed52de528986
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-22 09:14:22 -0300

    refactor: Change text, rename and add id

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,19 @@ import "../style/home/homeContent.css";
 
 function Home() {
   return (
-    <section id="home" className="home">
+    <section className="home">
       <div className="home__content">
         <h1 className="home__title">
           Olá, meu nome é <strong>Luan Victor</strong>
         </h1>
         <div className="home__info">
           <p className="home__info-text">
-            Estudo programação de forma autodidata. Desde criança sou
-            interessado no mundo da programação, passando por{" "}
-            <strong>desenvolvedor de jogos</strong> até{" "}
-            <strong>desenvolvedor web</strong>. Minhas práticas, no momento,
-            estão sendo para o <strong>Front-End</strong>, porém, por amar
-            sistemas de segurança e coisas relacionadas, não descarto a
-            importância do Back-End e pretendo estudá-lo assim que possível.
+            Um apaixonado por tecnologia que decidiu seguir o caminho da
+            programação.
           </p>
         </div>
         <div className="home__link-button">
-          <a href="#projeto" className="btn home__button">
+          <a href="#projects" className="btn home__button">
             Meus projetos
           </a>
         </div>
