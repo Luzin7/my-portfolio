8d2ab76b5ca9b857bb3fa29b5e155c0dc0e60445
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-25 00:52:03 -0300

    feat: Update infos at projects array

--- src/utils/getProjects.js
+++ src/utils/getProjects.js
@@ -1,20 +1,37 @@
+import weatherAppImg from "../assets/imgs/react-weather-app.jpg";
+
 export const PROJECTS = [
   {
     id: 1,
-    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    media: null,
     name: "Game-hub",
-    repository: "url",
+    repository: "https://github.com/Luzin7/game-hub",
+    description:
+      "Uma aplicação desenvolvida para prática da ferramenta React.JS utlizando React Router como principal aprendizado. No projeto você consegue ter informações de todos os jogos disponíveis, desde sua descrição até o link de download. Tudo dinamicamente.",
   },
   {
     id: 2,
-    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    media: weatherAppImg,
     name: "Weather App",
-    repository: "url",
+    repository: "https://github.com/Luzin7/react-weather-app",
+    description:
+      "Uma aplicação sobre clima(s), no qual é possível pesquisar cidades, visualizar sua temperatura e descrição sobre o tempo, além de adicionar aos seus favoritos. Tudo isso de forma dinâmica, usando formulários e o estado da aplicação.",
   },
   {
     id: 3,
-    media: "https://media4.giphy.com/media/ahC1kuLiarskCb2LvE/giphy.gif",
+    media: null,
     name: "Em breve",
     repository: "Em breve",
+    description: null,
+  },
+  {
+    id: 4,
+    media: null,
+    name: "Em breve",
+    repository: "Em breve",
+    description: null,
   },
 ];
+
+export const NO_MEDIA =
+  "https://media3.giphy.com/media/IbHjVrkArbNFNGPTpj/giphy.gif?cid=790b76119b49def524708146c2ac6dba9ae95295bcecfea8&rid=giphy.gif&ct=ts";
