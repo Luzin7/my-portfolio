68aad30e53cc5548fbb3115dd9a6a3511cf0cea3
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-25 00:56:07 -0300

    feat: Add Skills

---
+++ src/utils/getSkills.js
@@ -0,0 +1,59 @@
+
+
+import jsLogo from "../assets/icons/js.svg";
+import reactLogo from "../assets/icons/react.svg";
+import vuejsLogo from "../assets/icons/vuejs.svg";
+import htmlLogo from "../assets/icons/html.svg";
+import cssLogo from "../assets/icons/css.svg";
+import gitLogo from "../assets/icons/Git-Icon.png";
+import gitHubLogo from "../assets/icons/GitHub-Mark-120px-plus.png";
+import sassLogo from "../assets/icons/sass.svg";
+import vscodeLogo from "../assets/icons/vscode.svg";
+
+export const SKILLS = [
+  {
+    id: 1,
+    icon: jsLogo,
+    name: "Javascript",
+  },
+  {
+    id: 2,
+    icon: reactLogo,
+    name: "React.JS",
+  },
+  {
+    id: 3,
+    icon: vuejsLogo,
+    name: "Vue.JS",
+  },
+  {
+    id: 4,
+    icon: htmlLogo,
+    name: "HTML",
+  },
+  {
+    id: 5,
+    icon: cssLogo,
+    name: "CSS",
+  },
+  {
+    id: 6,
+    icon: gitLogo,
+    name: "Git",
+  },
+  {
+    id: 7,
+    icon: gitHubLogo,
+    name: "GitHub",
+  },
+  {
+    id: 8,
+    icon: sassLogo,
+    name: "Sass",
+  },
+  {
+    id: 9,
+    icon: vscodeLogo,
+    name: "VS Code",
+  },
+];
