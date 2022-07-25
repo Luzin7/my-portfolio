5082587796a14288ddd45c7a6d07670d85ffc684
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-25 00:55:30 -0300

    feat: Initiate Aos

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import './style/responsive.css'
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 700 });
+  });
   return (
     <div id="home" className="App">
       <Header />
       <Home />
       <About />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
