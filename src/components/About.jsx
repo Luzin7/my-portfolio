a72b1e4f94ac4f26dae5b47aec134fb28a704d9d
Luzin7 <lvictor.dutra@gmail.com>
2023-06-22 17:59:52 -0300

    update(About.jsx): altera informações e escrita

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,26 +13,26 @@ function About() {
         <h2 className="about__title">Sobre mim</h2>
         <p className="about__sub">
           Me chamo Luan Victor, tenho 20 anos e estudo desenvolvimento web
-          Front-end de forma autônoma. Tenho experiência com trabalho em equipe
-          e pretendo me tornar Full-stack.
+          Front-End de forma autodidata. Tenho experiência com trabalho em
+          equipe e pretendo me tornar Full-Stack.
         </p>
       </div>
       <div className="about__content">
         <div className="about__info">
           <h3 data-aos="fade-up" className="about__skills-title">
-            Minha carreira na programação
+            Por que programação?
           </h3>
           <p className="about__info-text">
             Desde criança sou interessado em tecnologia, sempre tive curiosidade
             sobre como fazer um projeto que eu achasse legal ou saber, por
-            exemplo, como um aplicativo ou um página web foram criados.
-            Por ter essa ambição de conhecimento já trilhei por algumas áreas da
+            exemplo, como um aplicativo ou um página web foram criados. Por ter
+            essa ambição de conhecimento já trilhei por algumas áreas da
             programação, desde desenvolvedor de jogos digitais até desenvolvedor
             web. Assim como qualquer pessoa, já vivi diferentes fases e gostos
             mas o interesse em programação sempre se manteve. Minhas práticas
             estão sendo para o Front-End, porém, por amar sistemas de segurança
-            e coisas relacionadas, não descarto a importância do Back-end e
-            pretendo estudá-lo assim que possível.
+            e coisas relacionadas, não descarto a importância do Back-end e já
+            estou estudando sobre essa área.
           </p>
         </div>
         <div className="about__skills">
